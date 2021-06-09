68feb4f19e5f738f84c906bdaaddc455ce3867c2
Cory Meyer <cameyer182@gmail.com>
2021-06-09 21:14:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,5 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def display_board(board)
  puts "   |   |   "
  puts "-----------"
@@ -5,5 +7,3 @@ def display_board(board)
  puts "-----------"
  puts "   |   |   "
 end
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
